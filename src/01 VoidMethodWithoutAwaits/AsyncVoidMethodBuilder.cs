6cfe7fefda1998ed73cc1a88b17b2d6f7a8789e5
Santiago Botero Correa <sabotero@gmail.com>
2020-01-23 15:20:13 +0100

    AsyncVoidMethodBuilder

---
+++ src/01 VoidMethodWithoutAwaits/AsyncVoidMethodBuilder.cs
@@ -0,0 +1,66 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    public struct AsyncVoidMethodBuilder
+    {
+#pragma warning disable CS0436 // Type conflicts with imported type
+        public static AsyncVoidMethodBuilder Create()
+        {
+            Console.WriteLine("Create called");
+            return new AsyncVoidMethodBuilder();
+        }
+
+        public void SetException(Exception e)
+        {
+            Console.WriteLine("SetException called");
+        }
+
+        public void SetResult()
+        {
+            Console.WriteLine("SetResult called");
+        }
+
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : System.Runtime.CompilerServices.INotifyCompletion
+            where TStateMachine : System.Runtime.CompilerServices.IAsyncStateMachine
+        {
+            Console.WriteLine("AwaitOnCompleted called");
+        }
+
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : System.Runtime.CompilerServices.ICriticalNotifyCompletion
+            where TStateMachine : System.Runtime.CompilerServices.IAsyncStateMachine
+        {
+            Console.WriteLine("AwaitUnsafeOnCompleted called");
+        }
+
+        public void Start<TStateMachine>(ref TStateMachine stateMachine)
+            where TStateMachine : System.Runtime.CompilerServices.IAsyncStateMachine
+        {
+            Console.WriteLine("Start called");
+        }
+
+        public void SetStateMachine(System.Runtime.CompilerServices.IAsyncStateMachine stateMachine)
+        {
+            Console.WriteLine("SetStateMachine called");
+        }
+    }
+
+    public interface INotifyCompletion
+    {
+        void OnCompleted(Action continuation);
+    }
+
+    public interface IAsyncStateMachine
+    {
+        void MoveNext();
+
+        void SetStateMachine(System.Runtime.CompilerServices.IAsyncStateMachine stateMachine);
+    }
+
+    public interface ICriticalNotifyCompletion : INotifyCompletion
+#pragma warning restore CS0436 // Type conflicts with imported type
+    {
+        [System.Security.SecurityCritical]
+        void UnsafeOnCompleted(Action continuation);
+    }
+}
---
+++ src/01 VoidMethodWithoutAwaits/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TrainingEduasync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DoNothingAsync();
+        }
+        
+        // Exactly the same method as in eduasyn, we get the same warning about the method missing an await operator
+        // but we do not have the compilation error
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        private static async void DoNothingAsync()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+        }
+    }
+}
